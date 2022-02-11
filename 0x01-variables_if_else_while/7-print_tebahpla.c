89c9899595b06a9e0afdb93cfc85fe29d3c4f686
xARKEINx <3884@holbertonschool.com>
2022-02-11 14:39:27 -0800

    reverce

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * a: variable
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	
+	for(a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	printf("\n");
+	return(0);
+}
