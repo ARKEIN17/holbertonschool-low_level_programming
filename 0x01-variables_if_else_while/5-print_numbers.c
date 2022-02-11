09a56c383e7b3a9ea217f1adbaaa59f348ff9512
xARKEINx <3884@holbertonschool.com>
2022-02-11 13:54:34 -0800

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ * a: variable
+*/
+int main(void)
+{
+	int a;
+
+			for (a = 0; a <= 9; a++)
+			{
+				printf("%d", a);
+			}
+			printf("\n");
+			return (0);
+}
