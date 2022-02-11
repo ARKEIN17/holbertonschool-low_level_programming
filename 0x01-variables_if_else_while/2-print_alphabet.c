cb6283464b6b5181e373ffe13fdba6088c13cc10
xARKEINx <3884@holbertonschool.com>
2022-02-11 12:58:30 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * a: variable
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+	for(a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	printf("\n");
+	return(0);
+}
