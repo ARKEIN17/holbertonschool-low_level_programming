b7c25204b727b5434f083438116c36436c9d307f
xARKEINx <3884@holbertonschool.com>
2022-03-09 19:19:32 -0800

    all

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - Funtion duplicate
+ *@str: Pointer
+ *a: my pointer
+ *b: Variable
+ *c: Variable
+ *Return: a
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int b, c;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; str[b] != '\0'; b++)
+		;
+	a = malloc((b + 1) * sizeof(*a));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; c < b; c++)
+	{
+		a[c] = str[c];
+	}
+	return (a);
+}
