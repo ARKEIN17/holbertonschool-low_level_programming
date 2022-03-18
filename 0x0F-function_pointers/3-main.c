f7cb38890b09ac33556c616001b10aa1a736ec20
xARKEINx <3884@holbertonschool.com>
2022-03-17 22:02:21 -0700

    all

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,26 +1,25 @@
 #include "3-calc.h"
 
 /**
- *  * main - function
- *   * @argc: number of arguments
- *    * @argv: arguments
- *     *
- *      * Return: NULL
- *       */
+ * main - function
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: NULL
+ */
 int main(int argc, char *argv[])
 {
-		int num1, num2;
-			int (*opera)(int, int);
+	int num1, num2;
+	int (*opera)(int, int);
 
-				if (argc == 4)
-						{
-									num1 = atoi(argv[1]);
-											num2 = atoi(argv[3]);
+	if (argc == 4)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+		opera = get_op_func(argv[2]);
+		printf("%d\n", opera(num1, num2));
 
-													opera = get_op_func(argv[2]);
-															printf("%d\n", opera(num1, num2));
-																	return (0);
-																		}
-					printf("Error\n");
-						exit(98);
+		return (0);
+	}
+	pintf("Error\n");
+	exit(98);
 }
