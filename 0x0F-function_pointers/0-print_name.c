a15fcb07163cb33a59e5c5fc700da48715084482
xARKEINx <3884@holbertonschool.com>
2022-03-17 20:12:16 -0700

    all

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function print name
+ * @name: pointer
+ * @f: Pointer to function
+ * Return: No Line
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+	{
+		f(name);
+	}
+}
