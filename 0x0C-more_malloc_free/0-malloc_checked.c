6b89d22561b20901cffa341c4674b55a3024197c
xARKEINx <3884@holbertonschool.com>
2022-03-17 19:00:07 -0700

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory
+ * @b: Variable
+ * p: pointer to b
+ * Return: a
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+
+	if (a == NULL)
+	{
+		exit(98);
+	}
+	return (a);
+}
