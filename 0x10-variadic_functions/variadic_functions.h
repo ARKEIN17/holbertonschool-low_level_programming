d8d533b1265b74318a1141b8ff95cd830073fc7c
xARKEINx <3884@holbertonschool.com>
2022-03-20 16:44:07 -0700

    all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,11 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void func_char(va_list ap);
+void func_int(va_list ap);
+void func_float(va_list ap);
+void funcp_char(va_list ap);
+void print_all(const char * const format, ...);
+
 
 #endif
