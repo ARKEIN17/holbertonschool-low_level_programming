b4691cf32ddf0f1be3141a176589e39b78d08fa9
xARKEINx <3884@holbertonschool.com>
2022-03-27 23:02:02 -0700

    2str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,48 +3,49 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Funtion concatenar
- * @s1: pointer to string 1
- * @s2: pointer to string 2
- * a: Pointer to return
- * b: path variable
- * c: path variable
- * d: concatenate variable
- * e: concatenate variable
- * Return: pointer a
+ * str_concat - that concatenates two strings
+ * @s1: string
+ * @s2: string
+ * Return: pointer
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *a;
-	int b, c, d, e;
+	int i = 0, j = 0, a = 0, b = 0;
+	char *ptr;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
-
-		if (s2 == NULL)
-		{
-			s2 = "";
-		}
-		for (b = 0; s1[b] != '\0'; b++)
-			;
-		for (c = 0; s2[c] != '\0'; c++)
-			;
-		a = malloc((b * sizeof(*s1)) + (c * sizeof(*s2)) + 1);
-
-		if (a == NULL)
-		{
-			return (NULL);
-		}
-		for (d = 0, e = 0; d < (b + c + 1); d++)
-			if (d < b)
-			{
-				a[d] = s1[d];
-			}
-			else
-			{
-				a[d] = s2[e++];
-			}
-		return (a);
 	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	ptr = malloc((i + j + 1) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (a < i)
+	{
+		ptr[a] = s1[a];
+		a++;
+	}
+	while (b < j)
+	{
+		ptr[a] = s2[b];
+		b++;
+		a++;
+	}
+	ptr[a] = '\0';
+	return (ptr);
+}
