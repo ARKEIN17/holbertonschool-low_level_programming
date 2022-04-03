455577765db00cd8b3fe052f6e72636d2ef57d63
xARKEINx <3884@holbertonschool.com>
2022-04-03 09:42:21 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function free
+ * @head: Double Pointer
+ * release: Variable
+ * Return: No line
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *release;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		release = *head;
+		*head = (*head)->next;
+		free(release);
+	}
+}
