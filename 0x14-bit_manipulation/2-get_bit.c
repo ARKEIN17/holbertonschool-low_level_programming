3aca6fc533a4035bf7d4e214cc346814622db186
xARKEINx <3884@holbertonschool.com>
2022-04-05 09:56:51 -0700

    return is the key

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,9 @@
 #include"main.h"
 /**
- *
+ * get_bit - Function
+ * @n: name variable
+ * @index: index starting in 0
+ * Return: -1 in case to wrong
  */
 
 int get_bit(unsigned long int n, unsigned int index)
@@ -9,8 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index <= bys)
 	{
-		return((n >> index & 1) + 0);
+		return ((n >> index & 1) + 0);
 	}
 
-	return(-1);
+	return (-1);
 }
