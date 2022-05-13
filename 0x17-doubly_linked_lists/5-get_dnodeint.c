1e8b4efc55d3ecc73d1ab640716b2fc9d015794f
xARKEINx <3884@holbertonschool.com>
2022-05-13 14:27:22 -0700

    get

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,20 +1,15 @@
 #include "lists.h"
 /**
  * get_dnodeint_at_index - get reurn index
- * @head: head of linked list
+ * * @head: head of linked list
  * @index: index
- * Return: index
+ *  * Return: index
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	while (i < index)
-	{
-		head = head->next;
-		i++;
-		if (head == NULL)
-			return (NULL);
-	}
-	return (head);
+	for (i = 0; head != NULL && i < index; i++)
+		head = head->next
+			return (head);
 }
