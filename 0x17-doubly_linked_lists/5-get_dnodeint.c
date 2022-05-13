172ef677a1ead206b2f3ae971e5b45364c63ae64
xARKEINx <3884@holbertonschool.com>
2022-05-13 14:32:19 -0700

    arkein

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,6 +10,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 
 	for (i = 0; head != NULL && i < index; i++)
-		head = head->next
+		head = head->next;
 			return (head);
 }
